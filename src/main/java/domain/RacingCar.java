28d4b80b4248d6e834ce80aa23693f8a5f401ca8
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-04 21:29:15 +0900

    feat : isWinner, compareTo 메서드 구현

--- src/main/java/domain/RacingCar.java
+++ src/main/java/domain/RacingCar.java
@@ -1,16 +1,25 @@
 package domain;
 
-public class RacingCar {
+public class RacingCar implements Comparable<RacingCar> {
 
     private int position;
 
     public void drive(DriveStrategy driveStrategy) {
         if (driveStrategy.driveable()) {
-           position++;
+            position++;
         }
     }
 
     public int position() {
         return this.position;
+    }
+
+    public boolean isWinner(int winnerPosition) {
+        return this.position >= winnerPosition;
+    }
+
+    @Override
+    public int compareTo(RacingCar obj) {
+        return Integer.compare(this.position, obj.position);
     }
 }
--- src/test/java/domain/RacingCarTest.java
+++ src/test/java/domain/RacingCarTest.java
@@ -2,6 +2,8 @@ package domain;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -31,5 +33,35 @@ class RacingCarTest {
 
         // then
         assertThat(racingCar.position()).isEqualTo(0);
+    }
+
+    @Test
+    @DisplayName("RacingCar 비교")
+    void car_compare() {
+        // given
+        RacingCar racingCar_move = new RacingCar();
+        RacingCar racingCar_stop = new RacingCar();
+
+        // when
+        racingCar_move.drive(() -> true);
+        racingCar_stop.drive(() -> false);
+
+        // then
+        assertThat(racingCar_move).isGreaterThan(racingCar_stop);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"2:true", "3:true", "4:false"}, delimiter = ':')
+    void car_winner(int winnerScore, boolean expected) {
+        // given
+        RacingCar racingCar = new RacingCar();
+
+        // when
+        racingCar.drive(() -> true);
+        racingCar.drive(() -> true);
+        racingCar.drive(() -> true);
+
+        // then
+        assertThat(racingCar.isWinner(winnerScore)).isEqualTo(expected);
     }
 }
