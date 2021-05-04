9bf4e49a025709cf22c17b5c161f9255ade9abf0
limdingdong <38725277+limdingdong@users.noreply.github.com>
2021-05-04 21:35:47 +0900

    feat : 경기결과 표현을 위한 dto 클래스 정의

---
+++ src/main/java/domain/dto/RoundResult.java
@@ -0,0 +1,16 @@
+package domain.dto;
+
+import java.util.List;
+
+public class RoundResult {
+
+    private final List<RoundScore> roundScores;
+
+    public RoundResult(List<RoundScore> roundScores) {
+        this.roundScores = roundScores;
+    }
+
+    public List<RoundScore> getRoundScores() {
+        return roundScores;
+    }
+}
---
+++ src/main/java/domain/dto/RoundScore.java
@@ -0,0 +1,20 @@
+package domain.dto;
+
+public class RoundScore {
+
+    private final String participantName;
+    private final int position;
+
+    public RoundScore(String participantName, int position) {
+        this.participantName = participantName;
+        this.position = position;
+    }
+
+    public String getParticipantName() {
+        return participantName;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
